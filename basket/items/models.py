968bc8f911403da760de494e8aa89aaf07f6f31f
gmanreviews <andrew.robinson@gmanreviews.com>
2016-12-08 18:43:34 -0500

    add item to admin
    Adding item object to model, database, and admin managed

--- basket/items/models.py
+++ basket/items/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class item(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=999,decimal_places=2)
+    pdf_location = models.CharField(max_length=200)
+    audio_location = models.CharField(max_length=200)
+    paperback = models.BooleanField(default=0)
+    pdf = models.BooleanField(default=0)
+    audio = models.BooleanField(default=0)
