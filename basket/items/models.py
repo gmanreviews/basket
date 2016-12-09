2367b62ccecc918fe3ef8651863a2c613cd26f6a
gmanreviews <andrew.robinson@gmanreviews.com>
2016-12-08 21:25:14 -0500

    more checks with item object

--- basket/items/models.py
+++ basket/items/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class item(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=999,decimal_places=2)
-    pdf_location = models.CharField(max_length=200)
-    audio_location = models.CharField(max_length=200)
+    pdf_location = models.CharField(max_length=200, blank=True)
+    audio_location = models.CharField(max_length=200, blank= True)
     paperback = models.BooleanField(default=0)
     pdf = models.BooleanField(default=0)
     audio = models.BooleanField(default=0)
--- basket/items/views.py
+++ basket/items/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import item
 # Create your views here.
 def index(request):
     return HttpResponse("You're viewing an item. I guess.")
+
+def all_items(request):
+    this_item = item.objects.all()
+    return render(request, 'items/all.html', {'items': this_item})
